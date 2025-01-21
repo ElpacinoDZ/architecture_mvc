17ab7e50786016e1ae3267bfc32e99b48a44c391
ElpacinoDZ <ahmedmoundji2014@outlook.fr>
2025-01-21 14:36:21 +0100

    Instancier le code

--- blog/index.php
+++ blog/index.php
@@ -4,6 +4,7 @@ require_once('src/controllers/add_comment.php');
 require_once('src/controllers/homepage.php');
 require_once('src/controllers/post.php');
 
+
 try {
     if (isset($_GET['action']) && $_GET['action'] !== '') {
         if ($_GET['action'] === 'post') {
